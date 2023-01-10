7bb76e342430db08c1fc6692100c33367429ecae
CHR <912215987@qq.com>
2023-01-10 18:52:06 +0800

    测试commit

--- src/Package01/Class01.java
+++ src/Package01/Class01.java
@@ -3,7 +3,7 @@ package Package01;
 public class Class01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-
+        System.out.println("Hello World");
     }
 
 }
